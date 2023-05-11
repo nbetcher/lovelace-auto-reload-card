d7277ff5f284083f4924fa9d31a6986d33565e3f
Nick Betcher <nbetcher@gmail.com>
2023-05-11 13:00:31 -0700

    Fix previous mistake in last commit; clean-up and format code.

--- auto-reload-card.js
+++ auto-reload-card.js
@@ -5,48 +5,44 @@ class AutoReloadCard extends HTMLElement {
 		const delay = this.config.delay_in_minute * 6e4;
 		
 		let sessionItem = window.AutoReloadCardIntervalHandle;
-		if(sessionItem) {
+		if (sessionItem) {
 			const [previousPanelUrl, previousIntervalHandle] = sessionItem.split(':');
-			if(previousPanelUrl !== hass.panelUrl) {
+			if (previousPanelUrl !== hass.panelUrl) {
 				clearInterval(previousIntervalHandle);
 				sessionItem = null;
 			}
 		}
-		if(!sessionItem) {
+
+		if (!sessionItem) {
 			const intervalHandle = setInterval(() => {
 				const homeAssistant = document.querySelector('home-assistant');
 				const root = homeAssistant.shadowRoot.querySelector('home-assistant-main').shadowRoot;
 				const panel = root.querySelector('ha-panel-lovelace');
-				if(panel) {
-					const uiRoot = panel.shadowRoot.querySelector('hui-root');
-					if(uiRoot) {
-						const header = uiRoot.shadowRoot.querySelector('div');
-						if (header) {
-							const isEditing = header.classList.contains('edit-mode');
-							if(isEditing)
-								return;
-						}
-					}
+				if (!panel) return;
+
+				const uiRoot = panel.shadowRoot.querySelector('hui-root');
+				if (uiRoot) {
+					const header = uiRoot.shadowRoot.querySelector('div');
+					if (header && header.classList.contains('edit-mode'))
+						return;
 				}
 			
 				const toolbar = uiRoot.shadowRoot.querySelector('app-toolbar');
 				const buttonMenu = toolbar.querySelector('ha-button-menu');
 				const refresh = buttonMenu.querySelector('[aria-label=Refresh]');
 			
-				if (refresh) {
+				if (refresh)
 					refresh.click();
-				} else {
+				else
 					location.reload();
-				}
 			}, delay);
 			window.AutoReloadCardIntervalHandle = `${hass.panelUrl}:${intervalHandle}`;
 		}
 	}
 
-
-
 	setConfig(config) {
-		if(config.delay_in_minute === undefined || typeof config.delay_in_minute !== 'number' || !Number.isInteger(config.delay_in_minute) || config.delay_in_minute < 1) {
+		if (config.delay_in_minute === undefined || typeof config.delay_in_minute !== 'number' || 
+		    !Number.isInteger(config.delay_in_minute) || config.delay_in_minute < 1) {
 			throw new Error('You need to define delay_in_minute (integer, starting from 1)');
 		}
 		this.config = config;
